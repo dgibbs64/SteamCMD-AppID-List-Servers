12967659d6a52cce74bc8a14111c0fae02e29993
Daniel Gibbs <me@danielgibbs.co.uk>
2018-09-07 00:05:11 +0100

    Copied SteamCMD-AppID-List repo

---
+++ .travis/push.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+git config --global user.email "me@danielgibbs.co.uk"
+git config --global user.name "dgibbs64"
+
+git remote set-url origin https://dgibbs64:${GH_TOKEN}@github.com/dgibbs64/SteamCMD-AppID-List.git
+
+git checkout ${TRAVIS_BRANCH}
+git add . steamcmd_appid.json
+git add . steamcmd_appid.csv
+git add . steamcmd_appid.md
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+
+
+git push --set-upstream origin $(date +%Y-%m-%d)
