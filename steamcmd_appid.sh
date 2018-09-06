d6efd2d29e5a9566587cd3c8224b2888ff30ee69
Daniel Gibbs <me@danielgibbs.co.uk>
2018-09-07 00:06:12 +0100

    updated to filter out the word server

--- steamcmd_appid.sh
+++ steamcmd_appid.sh
@@ -8,7 +8,7 @@
 rootdir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 echo "Creating steamcmd_appid.json"
-curl https://api.steampowered.com/ISteamApps/GetAppList/v2/ | jq -r '.' > steamcmd_appid.json
+curl https://api.steampowered.com/ISteamApps/GetAppList/v2/ | jq -c '.applist.apps[] | select(.name | contains("server","Server"))' > steamcmd_appid-servers.json
 echo "Creating steamcmd_appid.csv"
 cat steamcmd_appid.json | jq '.applist.apps[]' | jq -r '[.appid, .name] | @csv' > steamcmd_appid.csv
 cat steamcmd_appid.json | jq '.applist[]' | md-table > steamcmd_appid.md
