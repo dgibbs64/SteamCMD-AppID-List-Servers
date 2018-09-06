76b5eec77566ea17bdcd87ea9956eefaa0cf771c
Daniel Gibbs <me@danielgibbs.co.uk>
2018-09-07 00:38:15 +0100

    corrected name

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,9 +6,9 @@ git config --global user.name "dgibbs64"
 git remote set-url origin https://dgibbs64:${GH_TOKEN}@github.com/dgibbs64/SteamCMD-AppID-List-Servers.git
 
 git checkout ${TRAVIS_BRANCH}
-git add . steamcmd_appid-servers.json
-git add . steamcmd_appid-servers.csv
-git add . steamcmd_appid-servers.md
+git add . steamcmd_appid_servers.json
+git add . steamcmd_appid_servers.csv
+git add . steamcmd_appid_servers.md
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
 
---
+++ steamcmd_appid_servers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# steamcmd_appid_servers.sh
+# Author: Daniel Gibbs
+# Website: http://danielgibbs.co.uk
+# Version: 180826
+# Description: Saves the complete list of all the appid their names in json and csv.
+
+rootdir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+echo "Creating steamcmd_appid_servers.json"
+curl https://api.steampowered.com/ISteamApps/GetAppList/v2/ | jq -c '.applist.apps[] | select(.name | contains("server","Server"))' > steamcmd_appid_servers.json
+echo "Creating steamcmd_appid_servers.csv"
+cat steamcmd_appid_servers.json | jq '.applist.apps[]' | jq -r '[.appid, .name] | @csv' > steamcmd_appid_servers.csv
+echo "Creating steamcmd_appid_servers.md"
+cat steamcmd_appid_servers.json | jq '.applist[]' | md-table > steamcmd_appid_servers.md
+echo "exit"
+exit
