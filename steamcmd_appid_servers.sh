eef13efc41413174763e59cc9bb2a6fcc1bc5bf0
BourneID <cam40902@gmail.com>
2018-09-22 21:25:50 +0100

    Downloading GetAppList now checks the HTTP code to ensure it's a 2xx, a retries on failure.

--- steamcmd_appid_servers.sh
+++ steamcmd_appid_servers.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 # steamcmd_appid_servers.sh
-# Author: Daniel Gibbs
+# Author: Daniel Gibbs & Robin Bourne
 # Website: http://danielgibbs.co.uk
-# Version: 180826
+# Version: 180922
 # Description: Saves the complete list of all the appid their names in json and csv.
 
+#declare variables
 rootdir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+attempt_counter=0
+max_attempts=5
+time_to_wait=30
+
+echo "Downloading temporary file steamcmd_appid_list.json"
+until $(curl --fail -o steamcmd_appid_list.json https://api.steampowered.com/ISteamApps/GetAppList/v2/); do
+  if [ ${attempt_counter} -eq ${max_attempts} ];then
+      echo "Max attempts reached. Aborting"
+      exit 1
+    fi
+    attempt_counter=$(($attempt_counter+1))
+    echo "Download failed (attempt ${attempt_counter} of ${max_attempts}). Waiting ${time_to_wait} seconds until trying again"
+
+  sleep ${time_to_wait}
+done
 
 echo "Creating steamcmd_appid_servers.json"
-curl https://api.steampowered.com/ISteamApps/GetAppList/v2/ | jq '[.applist.apps[] | select(.name | contains("server","Server"))]' > steamcmd_appid_servers.json
+cat steamcmd_appid_list.json | jq '[.applist.apps[] | select(.name | contains("server","Server"))]' > steamcmd_appid_servers.json
+
 echo "Creating steamcmd_appid_servers.csv"
 cat steamcmd_appid_servers.json | jq -r '.[] | [.appid, .name] | @csv' > steamcmd_appid_servers.csv
+
 echo "Creating steamcmd_appid_servers.md"
 cat steamcmd_appid_servers.json | md-table > steamcmd_appid_servers.md
+
 echo "exit"
 exit
