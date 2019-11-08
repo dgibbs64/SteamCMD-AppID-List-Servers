736e567de92980b75f3c7dda3f6f10a6eb84dafc
Daniel Gibbs <me@danielgibbs.co.uk>
2019-11-08 00:32:52 +0000

    remove dot

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,9 +6,9 @@ git config --global user.name "dgibbs64"
 git remote set-url origin https://dgibbs64:${GH_TOKEN}@github.com/dgibbs64/SteamCMD-AppID-List-Servers.git
 
 git checkout ${TRAVIS_BRANCH}
-git add . steamcmd_appid_servers.json
-git add . steamcmd_appid_servers.csv
-git add . steamcmd_appid_servers.md
+git add steamcmd_appid_servers.json
+git add steamcmd_appid_servers.csv
+git add steamcmd_appid_servers.md
 git commit --message "Travis build: $(date +%Y-%m-%d)"
 
 git push --set-upstream origin ${TRAVIS_BRANCH}
