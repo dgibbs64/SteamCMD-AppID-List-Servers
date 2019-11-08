d2e3273bca282471b5aa8363f95d840996539bbe
Daniel Gibbs <me@danielgibbs.co.uk>
2019-11-08 11:52:07 +0000

    Update steamcmd_appid_servers.sh

--- steamcmd_appid_servers.sh
+++ steamcmd_appid_servers.sh
@@ -161,7 +161,6 @@ echo "Creating steamcmd_appid_servers.md"
 cat steamcmd_appid_servers.json | md-table > steamcmd_appid_servers.md
 
 cat steamcmd_appid_servers.json | jq '[.[] | select(.linux == true)]' | jq -s '.[]|sort_by(.appid)' > steamcmd_appid_servers_linux.json
-echo "${steam_servers_linux}" > 
 
 echo "Creating steamcmd_appid_servers_linux.csv"
 cat steamcmd_appid_servers_linux.json | jq -r '.[] | [.appid, .name, .subscription, .linux, .windows] | @csv' > steamcmd_appid_servers.csv
