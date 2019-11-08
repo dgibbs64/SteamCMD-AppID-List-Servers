41310bdbae114f9fc19482d2f595cb21ec2ea655
Daniel Gibbs <me@danielgibbs.co.uk>
2019-11-08 00:13:35 +0000

    added linux and windows details

--- steamcmd_appid_servers.sh
+++ steamcmd_appid_servers.sh
@@ -5,31 +5,92 @@
 # Version: 180922
 # Description: Saves the complete list of all the appid their names in json and csv.
 
-#declare variables
-attempt_counter=0
-max_attempts=5
-time_to_wait=30
+# Set TMUX_SESSIONS For development
+if [ ! -v TMUX_SESSIONS ]; then
+	TMUX_SESSIONS=7
+fi
 
-echo "Downloading temporary file steamcmd_appid_list.json"
-until $(curl --fail -o steamcmd_appid_list.json https://api.steampowered.com/ISteamApps/GetAppList/v2/); do
-  if [ ${attempt_counter} -eq ${max_attempts} ];then
-      echo "Max attempts reached. Aborting"
-      exit 1
-    fi
-    attempt_counter=$((attempt_counter+1))
-    echo "Download failed (attempt ${attempt_counter} of ${max_attempts}). Waiting ${time_to_wait} seconds until trying again"
+# Static variables
+rootdir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-  sleep ${time_to_wait}
-done
+# Downloads the source data files for analysis.
+download_steam_files() {
+		echo "Creating steamcmd_appid.json"
+		curl https://api.steampowered.com/ISteamApps/GetAppList/v2/ | jq -r '.' > steamcmd_getapplist.json
+}
+
+# Checks for SteamCMD, and installs if it does not exist.
+install_steamcmd(){
+		echo ""
+		echo "Installing SteamCMD."
+		cd "${rootdir}" || exit
+		mkdir -pv "steamcmd"
+		cd "steamcmd" || exit
+		if [ ! -f "steamcmd.sh" ]; then
+				echo -e "downloading steamcmd_linux.tar.gz...\c"
+				wget -N /dev/null http://media.steampowered.com/client/steamcmd_linux.tar.gz 2>&1 | grep -F HTTP | cut -c45-| uniq
+				tar --verbose -zxf "steamcmd_linux.tar.gz"
+				rm -v "steamcmd_linux.tar.gz"
+				chmod +x "steamcmd.sh"
+		else
+				echo "SteamCMD is already installed."
+		fi
+		cd "${rootdir}" || exit
+}
+
+# Generate a list of commands to send to SteamCMD.
+# Parameter 1: JSON content to parse as an array of relevant entities.
+# Returns: Output as string.
+generate_commands() {
+		local input_json="${1}"
+		local temp_file="$(mktemp)"
+		echo "${input_json}" > "${temp_file}"
+
+		local output="$(jq -n -f "${temp_file}" | jq -r '.[] | [.appid] | @csv' | sed 's/^/tmux send-keys "app_status /' | sed 's/$/" ENTER/')"
+
+		echo "${output}"
+		rm "${temp_file}"
+}
+
+# check required external variables.
+if [ -z "${TMUX_SESSIONS+x}" ]; then
+	echo "TMUX_SESSIONS is not set. Please set this variable to allocate the number of TMUX sessions that should be used to query SteamCMD."
+	exit 1
+fi
+
+# pre-requirements.
+install_steamcmd
+download_steam_files
 
 echo "Creating steamcmd_appid_servers.json"
-cat steamcmd_appid_list.json | jq '[.applist.apps[] | select(.name | contains("server","Server"))]'| jq -s '.[]|sort_by(.appid)' > steamcmd_appid_servers.json
+cat steamcmd_getapplist.json | jq '[.applist.apps[] | select(.name | contains("server","Server"))]'| jq -s '.[]|sort_by(.appid)' > steamcmd_appid_servers.json
 
 echo "Creating steamcmd_appid_servers.csv"
 cat steamcmd_appid_servers.json | jq -r '.[] | [.appid, .name] | @csv' > steamcmd_appid_servers.csv
 
 echo "Creating steamcmd_appid_servers.md"
 cat steamcmd_appid_servers.json | md-table > steamcmd_appid_servers.md
+
+# Checks for SteamCMD, and installs if it does not exist.
+install_steamcmd(){
+		echo ""
+		echo "Installing SteamCMD."
+		cd "${rootdir}" || exit
+		mkdir -pv "steamcmd"
+		cd "steamcmd" || exit
+		if [ ! -f "steamcmd.sh" ]; then
+				echo -e "downloading steamcmd_linux.tar.gz...\c"
+				wget -N /dev/null http://media.steampowered.com/client/steamcmd_linux.tar.gz 2>&1 | grep -F HTTP | cut -c45-| uniq
+				tar --verbose -zxf "steamcmd_linux.tar.gz"
+				rm -v "steamcmd_linux.tar.gz"
+				chmod +x "steamcmd.sh"
+		else
+				echo "SteamCMD is already installed."
+		fi
+		cd "${rootdir}" || exit
+}
+
+
 
 echo "exit"
 exit
