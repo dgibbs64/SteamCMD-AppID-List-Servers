4cb04b5bb6d35cd3c1ac1ad50503ed41c4d32099
Daniel Gibbs <me@danielgibbs.co.uk>
2022-08-18 17:43:41 +0100

    executable

--- steamcmd_appid_servers.sh
+++ steamcmd_appid_servers.sh
@@ -10,41 +10,41 @@ rootdir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 # Downloads the source data files for analysis.
 download_steam_files() {
-	if [ ! -f "steamcmd_getapplist.json" ]; then
-		echo "Creating steamcmd_appid.json"
-		curl https://api.steampowered.com/ISteamApps/GetAppList/v2/ | jq -r '.' > steamcmd_getapplist.json
-	fi
+  if [ ! -f "steamcmd_getapplist.json" ]; then
+    echo "Creating steamcmd_appid.json"
+    curl https://api.steampowered.com/ISteamApps/GetAppList/v2/ | jq -r '.' > steamcmd_getapplist.json
+  fi
 }
 
 # Checks for SteamCMD, and installs if it does not exist.
-install_steamcmd(){
-	echo ""
-	echo "Installing SteamCMD."
-	cd "${rootdir}" || exit
-	mkdir -pv "steamcmd"
-	cd "steamcmd" || exit
-	if [ ! -f "steamcmd.sh" ]; then
-			echo -e "downloading steamcmd_linux.tar.gz...\c"
-			wget -N /dev/null http://media.steampowered.com/client/steamcmd_linux.tar.gz 2>&1 | grep -F HTTP | cut -c45-| uniq
-			tar --verbose -zxf "steamcmd_linux.tar.gz"
-			rm -v "steamcmd_linux.tar.gz"
-			chmod +x "steamcmd.sh"
-	else
-			echo "SteamCMD is already installed."
-	fi
-	cd "${rootdir}" || exit
+install_steamcmd() {
+  echo ""
+  echo "Installing SteamCMD."
+  cd "${rootdir}" || exit
+  mkdir -pv "steamcmd"
+  cd "steamcmd" || exit
+  if [ ! -f "steamcmd.sh" ]; then
+    echo -e "downloading steamcmd_linux.tar.gz...\c"
+    wget -N /dev/null http://media.steampowered.com/client/steamcmd_linux.tar.gz 2>&1 | grep -F HTTP | cut -c45- | uniq
+    tar --verbose -zxf "steamcmd_linux.tar.gz"
+    rm -v "steamcmd_linux.tar.gz"
+    chmod +x "steamcmd.sh"
+  else
+    echo "SteamCMD is already installed."
+  fi
+  cd "${rootdir}" || exit
 }
 
 # Generate a list of commands to send to SteamCMD.
 # Parameter 1: JSON content to parse as an array of relevant entities.
 # Returns: Output as string.
 generate_commands() {
-	local input_json="${1}"
-	local temp_file="$(mktemp)"
-	echo "${input_json}" > "${temp_file}"
-	local output="$(jq -n -f "${temp_file}" | jq -r '.[] | [.appid] | @csv' | sed 's/^/tmux send-keys "app_status /' | sed 's/$/" ENTER/')"
-	echo "${output}"
-	rm "${temp_file}"
+  local input_json="${1}"
+  local temp_file="$(mktemp)"
+  echo "${input_json}" > "${temp_file}"
+  local output="$(jq -n -f "${temp_file}" | jq -r '.[] | [.appid] | @csv' | sed 's/^/tmux send-keys "app_status /' | sed 's/$/" ENTER/')"
+  echo "${output}"
+  rm "${temp_file}"
 }
 
 # pre-requirements.
@@ -52,7 +52,7 @@ install_steamcmd
 download_steam_files
 
 echo "Creating steamcmd_appid_servers.json"
-steam_servers=$(cat steamcmd_getapplist.json | jq '[.applist.apps[] | select(.name | contains("server","Server"))]'| jq -s '.[]|sort_by(.appid)')
+steam_servers=$(cat steamcmd_getapplist.json | jq '[.applist.apps[] | select(.name | contains("server","Server"))]' | jq -s '.[]|sort_by(.appid)')
 echo "${steam_servers}" > steamcmd_appid_servers.json
 
 echo "Generate tmux script to check servers platform."
@@ -65,14 +65,14 @@ echo "tmux send-keys -t tmux-linux \"exit\" ENTER" >> tmux_steam_server_commands
 
 echo "Spin up 1 TMUX in Linux mode."
 if [ ! -f "tmux_steam_server_output_linux.txt" ]; then
-	touch "tmux_steam_server_output_linux.txt"
+  touch "tmux_steam_server_output_linux.txt"
 fi
 tmux new -s "tmux-linux" -d './steamcmd/steamcmd.sh +login anonymous' \; pipe-pane "cat > ./tmux_steam_server_output_linux.txt"
 echo ""
 echo "Waiting for SteamCMD prompt."
 while ! grep -q "Steam>" tmux_steam_server_output_linux.txt; do
-	echo -n "."
-	sleep 1
+  echo -n "."
+  sleep 1
 done
 
 # executing commands to windows shell and awaiting finish.
@@ -82,8 +82,8 @@ done
 echo ""
 echo "Waiting for the tmux session to finish."
 while [ "$(tmux ls | wc -l)" -ne "0" ]; do
-	echo -n "."
-	sleep 1
+  echo -n "."
+  sleep 1
 done
 
 echo "Generate tmux script to check servers platform."
@@ -96,26 +96,25 @@ echo "tmux send-keys -t tmux-windows \"exit\" ENTER" >> tmux_steam_server_comman
 
 echo "Spin up 1 TMUX in Windows mode."
 if [ ! -f "tmux_steam_server_output_windows.txt" ]; then
-	touch "tmux_steam_server_output_windows.txt"
+  touch "tmux_steam_server_output_windows.txt"
 fi
 tmux new -s "tmux-windows" -d './steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous' \; pipe-pane "cat > ./tmux_steam_server_output_windows.txt"
 echo ""
 echo "Waiting for SteamCMD prompt."
 while ! grep -q "Steam>" tmux_steam_server_output_windows.txt; do
-	echo -n "."
-	sleep 1
+  echo -n "."
+  sleep 1
 done
 
 # executing commands to windows shell and awaiting finish.
 ./tmux_steam_server_commands_windows.sh &
 
-
 # wait for the tmux session to finish
 echo ""
 echo "Waiting for the tmux session to finish."
-while [ "$(tmux ls | wc -l)" -ne "0" ];	do
-	echo -n "."
-	sleep 1
+while [ "$(tmux ls | wc -l)" -ne "0" ]; do
+  echo -n "."
+  sleep 1
 done
 
 pcre2grep -M -o1 -o2 --om-separator=\; 'AppID ([0-9]{1,8})[\s\S]*?release state: (.*)$' tmux_steam_server_output_linux.txt > tmux_steam_server_linux.csv
